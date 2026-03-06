51675790939d2923fb35843eb3c36525c200635d
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-03-06 15:20:35 +0200

    feat: Add a file to define types we will be using throughtout the application

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,16 @@ export interface TodoList {
   task: string;
 }
 
+export interface DestinationData {
+  id: number,
+    img: {
+      src: string,
+      alt: string
+    },
+    title: string
+    country: string,
+    googleMapsLink: string,
+    dates: string,
+    text: string,
+  
+}
