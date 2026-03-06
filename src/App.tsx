3a08c3199095dd90464ef8ff88382fc63ed84150
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-03-06 15:21:37 +0200

    feat: Refactor App component to manage user position and display location details

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,52 @@
+import { useImmer } from "use-immer";
+import ArraysNesting from "./practice/ArraysNesting";
+import Todos from "./practice/ReducerPractice";
+
 function App() {
-  return <h1>fetching....</h1>;
+  const [position, updatePosition] = useImmer({
+    name: "",
+    age: 0,
+    location: {
+      street: "kk454st",
+      city: "kigali",
+      country: "Rwanda",
+    },
+  });
+  return (
+    <>
+      <form action="">
+        <input
+          type="text"
+          value={position.name}
+          // onChange={(e) => setPosition({ ...position, name: e.target.value })}
+        />
+        <input
+          type="text"
+          value={+position.age}
+          // onChange={(e) => setPosition({ ...position, age: +e.target.value })}
+        />
+      </form>
+      <div className="details">
+        <p>names: {position.name}</p>
+        <p>names: {position.age}</p>
+        <h2>location</h2>
+        <div>
+          <p>street: {position.location.street}</p>
+          <p>city: {position.location.city}</p>
+          <p>country: {position.location.country}</p>
+        </div>
+      </div>
+      <button
+        onClick={() => {
+          updatePosition((see) => {
+            see.location.city = "Rusizi";
+          });
+        }}
+      >
+        change address info
+      </button>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from "./App.tsx";
+// import LayoutAndUseEffectDiff from "./practice/LayoutAndUseEffectDiff.tsx";
+// import CounterReduce from "./practice/ReducerPractice.tsx";
+// import Count from "./practice/Count.tsx";
+import ContextPractice from "./practice/ContextPractice.tsx";
+import CountryProvider from "./context/ContextProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-   </StrictMode>,
-)
+    <CountryProvider>
+      {/* <App /> */}
+      <ContextPractice />
+    </CountryProvider>
+  </StrictMode>,
+);
