dc5c7d145e8ec35b933a38ef1ffafc0890e3760b
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-03-07 11:58:58 +0200

    feat:Practicing rendering data dynamically and using context to get data from the Top parents

--- src/practice/CounterDisplay1.tsx
+++ src/practice/CounterDisplay1.tsx
@@ -1,12 +1,36 @@
 import { useContext } from "react";
-
-// import type { DestinationData } from "../types";
 import { CountryData } from "../context/ContextProvider";
+import type { DestinationData } from "../types";
 
 function CounterDisplay1() {
   const { data } = useContext(CountryData);
   console.log(data);
-  return <div>CounterDisplay1</div>;
+  return (
+    <>
+      {data.map((element: DestinationData, index: number) => {
+        return (
+          <div key={index}>
+            <DisplayData {...element} />
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+function DisplayData(props: DestinationData) {
+  return (
+    <>
+      <img src={props.img.src} alt={props.img.alt} />
+      <div className="details">
+        <h2>{props.country}</h2>
+        <h3>{props.title}</h3>
+        <p>text: {props.text}</p>
+        <p>Map: {props.googleMapsLink}</p>
+        <p>Dates: {props.dates}</p>
+      </div>
+    </>
+  );
 }
 
 export default CounterDisplay1;
