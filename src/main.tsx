6a556c59b02ce45b5ae3aaeff5c9e36ea111d241
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-03-12 18:39:41 +0200

    feat:Refactor main.tsx to use Calculator component and remove unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import Calculator from "./calculatorExercises/Calculator";
 // import App from "./App.tsx";
 // import LayoutAndUseEffectDiff from "./practice/LayoutAndUseEffectDiff.tsx";
 // import CounterReduce from "./practice/ReducerPractice.tsx";
 // import Count from "./practice/Count.tsx";
-import ContextPractice from "./practice/ContextPractice.tsx";
-import CountryProvider from "./context/ContextProvider.tsx";
+// import ContextPractice from "./practice/ContextPractice.tsx";
+// import CountryProvider from "./context/ContextProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CountryProvider>
-      {/* <App /> */}
-      <ContextPractice />
-    </CountryProvider>
+    <Calculator />
   </StrictMode>,
 );
