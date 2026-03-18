5fffb2e1a375307787ed4ade7a774c0074208fb5
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-03-18 20:10:56 +0200

    feat: Refactor App component to use routing and remove unused state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,18 @@
-import { useImmer } from "use-immer";
-import ArraysNesting from "./practice/ArraysNesting";
-import Todos from "./practice/ReducerPractice";
+import { Routes, Route, Link } from "react-router-dom";
+import Popup from "./practice/Popup";
+import Todos from "./practice/Todos";
 
 function App() {
-  const [position, updatePosition] = useImmer({
-    name: "",
-    age: 0,
-    location: {
-      street: "kk454st",
-      city: "kigali",
-      country: "Rwanda",
-    },
-  });
   return (
     <>
-      <form action="">
-        <input
-          type="text"
-          value={position.name}
-          // onChange={(e) => setPosition({ ...position, name: e.target.value })}
-        />
-        <input
-          type="text"
-          value={+position.age}
-          // onChange={(e) => setPosition({ ...position, age: +e.target.value })}
-        />
-      </form>
-      <div className="details">
-        <p>names: {position.name}</p>
-        <p>names: {position.age}</p>
-        <h2>location</h2>
-        <div>
-          <p>street: {position.location.street}</p>
-          <p>city: {position.location.city}</p>
-          <p>country: {position.location.country}</p>
-        </div>
-      </div>
-      <button
-        onClick={() => {
-          updatePosition((see) => {
-            see.location.city = "Rusizi";
-          });
-        }}
-      >
-        change address info
-      </button>
+      <Link to="/">Home</Link>
+      
+      <Link to="/popup">Pop</Link>
+      <Routes>
+        <Route path="/" element={<Todos />} />
+
+        <Route path="/popup" element={<Popup />} />
+      </Routes>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Calculator from "./calculatorExercises/Calculator";
-// import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
 // import LayoutAndUseEffectDiff from "./practice/LayoutAndUseEffectDiff.tsx";
 // import CounterReduce from "./practice/ReducerPractice.tsx";
 // import Count from "./practice/Count.tsx";
@@ -10,7 +11,9 @@ import Calculator from "./calculatorExercises/Calculator";
 // import CountryProvider from "./context/ContextProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Calculator />
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>,
 );
