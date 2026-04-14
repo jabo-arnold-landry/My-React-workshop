7c026f3be0f29337db496abb349832bb467989c6
jabo <arnoldjabo@gmail.com>
2026-04-14 10:11:49 +0200

    fix: Add type annotation for children prop in CountryProvider component

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -1,9 +1,12 @@
-import { createContext, useState } from "react";
+import { createContext, useState, type ReactNode } from "react";
 import type { DestinationData } from "../types";
 
 export const CountryData = createContext<any>(undefined);
+interface ChildProp {
+  children: ReactNode;
+}
 
-export default function CountryProvider({ children }) {
+export default function CountryProvider({ children }: ChildProp) {
   const [data, setData] = useState<DestinationData[]>([]);
 
   return (
