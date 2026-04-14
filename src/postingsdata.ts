ec6ff33c153495d1163586bece53a20cb7d8e819
jabo <arnoldjabo@gmail.com>
2026-04-14 19:26:44 +0200

    feat: Add postings data for individual postings

---
+++ src/postingsdata.ts
@@ -0,0 +1,33 @@
+const postings = [
+  {
+    id: 1,
+    name: "work",
+    daily: "day",
+    description: "lorem",
+  },
+  {
+    id: 2,
+    name: "dashboard",
+    daily: "night",
+    description: "lorem",
+  },
+  {
+    id: 3,
+    name: "fine",
+    daily: "day/night",
+    description: "lorem",
+  },
+  {
+    id: 4,
+    name: "fire",
+    daily: "day",
+    description: "lorem",
+  },
+  {
+    id: 5,
+    name: "hi",
+    daily: "night",
+    description: "lorem",
+  },
+];
+export default postings;
