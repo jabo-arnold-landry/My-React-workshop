7b7bf132b168201834b53e184c6b8b963ce19d40
jabo <arnoldjabo@gmail.com>
2026-04-14 19:25:14 +0200

    add more components for nesting and testing for nesting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { lazy } from "react";
 import { NavLink, Route, Routes } from "react-router-dom";
+
+const RouterErrorBoundary = lazy(
+  () => import("./RouterPractice/RouterErrorBoundary"),
+);
 const Home = lazy(() => import("./RouterPractice/Home"));
 const About = lazy(() => import("./RouterPractice/About"));
 const Postings = lazy(() => import("./RouterPractice/Postings"));
+const PostLayout = lazy(() => import("./RouterPractice/PostLayout"));
+const Post = lazy(() => import("./RouterPractice/Post"));
 
 function App() {
   return (
@@ -15,7 +21,14 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/postings" element={<Postings />} />
+        <Route path="postings" element={<PostLayout />}>
+          <Route index element={<Postings />} />
+          <Route
+            path=":id"
+            errorElement={<RouterErrorBoundary />}
+            element={<Post />}
+          />
+        </Route>
       </Routes>
     </>
   );
