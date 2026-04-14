1ddd8772a12f6c90f4ddbea01324b62f10932d14
jabo <arnoldjabo@gmail.com>
2026-04-14 10:11:27 +0200

    refactor: Clean up App and main files by removing unused imports and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import Todos from "./practice/Todos";
-import TestErrorBound from "./practice/TestErrorBound";
-import ErrorBoundary from "./practice/ErrorBoundary";
-
 function App() {
-  return (
-    <>
-      <TestErrorBound heroName="landry" />
-      <TestErrorBound heroName="jabo" />
-      <ErrorBoundary fallback={<p>fuck you</p>}>
-        <Todos />
-      </ErrorBoundary>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
