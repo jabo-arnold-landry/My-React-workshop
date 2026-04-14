09683c451a2800d85c1167c2de20a2bc737df033
jabo <arnoldjabo@gmail.com>
2026-04-14 10:58:16 +0200

    Add starter files and snippet to start practicing about router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { lazy } from "react";
+import { NavLink, Route, Routes } from "react-router-dom";
+const Home = lazy(() => import("./RouterPractice/Home"));
+const About = lazy(() => import("./RouterPractice/About"));
+const Postings = lazy(() => import("./RouterPractice/Postings"));
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/postings">Postings</NavLink>
+      </div>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/postings" element={<Postings />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/RouterPractice/Home.tsx
@@ -0,0 +1,18 @@
+function About() {
+  return (
+    <>
+      <h1>Home Page</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        dolores iste perspiciatis dolorum optio veniam odio recusandae corporis,
+        consequuntur repellendus, obcaecati minima vitae velit rem! Dicta
+        incidunt consequuntur molestiae architecto atque, nostrum dolore aperiam
+        recusandae impedit qui et dignissimos accusamus, iste quibusdam
+        repudiandae. Distinctio, similique.
+      </p>
+    </>
+  );
+}
+
+export default About;
