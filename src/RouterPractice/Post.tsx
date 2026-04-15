ce2fd44c1fd2bca028ea37d851c468f126def65c
jabo <arnoldjabo@gmail.com>
2026-04-15 11:00:43 +0200

    Practice using context of outlet

--- src/RouterPractice/Post.tsx
+++ src/RouterPractice/Post.tsx
@@ -1,14 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useParams, useOutletContext } from "react-router-dom";
 import postings from "../postingsdata";
+type OutletContext = {
+  counter: number;
+  setCounter: React.Dispatch<React.SetStateAction<number>>;
+};
 function Post() {
   const { id } = useParams();
   const data = postings[id];
+  const {counter}= useOutletContext<OutletContext>();
   if (data.description == undefined || data.description == null) {
     throw Error("wrong id");
   }
   return (
     <>
       <h4>{data.description}</h4>
+      <button>{counter}</button>
     </>
   );
 }
--- src/RouterPractice/PostLayout.tsx
+++ src/RouterPractice/PostLayout.tsx
@@ -1,17 +1,21 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate, useLocation } from "react-router-dom";
+import { useState } from "react";
 
 function PostLayout() {
+  const [counter, setCounter] = useState(0);
   const user = true;
+  const location = useLocation();
   return (
     <>
       <h1>Postings Page</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>{counter}</button>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium,
         recusandae.
       </p>
 
       {user ? (
-        <Outlet />
+        <Outlet context={{ counter }} />
       ) : (
         <Navigate to="/" replace={true} state={{ from: location.pathname }} />
       )}
