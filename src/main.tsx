8a21e729f5b27743fbf511e3f50cddebc1aceebd
jabo <arnoldjabo@gmail.com>
2026-04-27 23:25:18 +0200

    consumed the store value using  the provider from redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import store from "./redux/store/counterStore.ts";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <StrictMode>
-      <App/>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </StrictMode>
   </BrowserRouter>,
 );
