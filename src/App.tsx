adba0800fdf1341fdb915c2eb3b91b18fe584821
jabo <arnoldjabo@gmail.com>
2026-04-28 19:57:45 +0200

    replace router practice with redux practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,17 @@
-import { lazy } from "react";
-import { NavLink, Route, Routes } from "react-router-dom";
+import CounterProvider from "./context/Counter";
+import CounteComp from "./redux/CounteComp";
+import Something from "./redux/Something";
 
-const RouterErrorBoundary = lazy(
-  () => import("./RouterPractice/RouterErrorBoundary"),
-);
-const Home = lazy(() => import("./RouterPractice/Home"));
-const About = lazy(() => import("./RouterPractice/About"));
-const Postings = lazy(() => import("./RouterPractice/Postings"));
-const PostLayout = lazy(() => import("./RouterPractice/PostLayout"));
-const Post = lazy(() => import("./RouterPractice/Post"));
-const Profile = lazy(() => import("./RouterPractice/Profile"));
-const AccData = lazy(() => import("./RouterPractice/AccData"));
+import UseOptimisticPractice from "./practice/UseOptimisticPractice";
 
 function App() {
   return (
     <>
-      <div>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/postings">Postings</NavLink>
-        <NavLink to="/profile">Profile</NavLink>
-      </div>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="postings" element={<PostLayout />}>
-          <Route index element={<Postings />} />
-          <Route
-            path=":id"
-            errorElement={<RouterErrorBoundary />}
-            element={<Post />}
-          />
-        </Route>
-        <Route path="profile" element={<Profile />}>
-          <Route index element={<AccData />} />
-        </Route>
-      </Routes>
+      <CounterProvider>
+        <CounteComp />
+        <Something />
+      </CounterProvider>
+      {/* <UseOptimisticPractice /> */}
     </>
   );
 }
