6ad4cbaa98e2911c721f6987f8be63534efe02ad
jabo <arnoldjabo@gmail.com>
2026-05-01 13:53:22 +0200

    added a store for fetching and adding data to the server using redux toolkit query library

--- src/redux/store/counterStore.ts
+++ src/redux/store/counterStore.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../slices/counterSlice";
 import userReducer from "../slices/userSlice";
+import { apiCreated } from "../slices/apiSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     user: userReducer,
+    [apiCreated.reducerPath]: apiCreated.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiCreated.middleware),
 });
 
 export type DispatchType = typeof store.dispatch;
