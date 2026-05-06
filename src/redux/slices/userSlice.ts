6cef3d6c41d666fb3a803073a5184823171d24b3
jabo <arnoldjabo@gmail.com>
2026-05-06 14:36:27 +0200

    feat: add newUser action to userSlice and update addLocation type for better type safety

--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -1,25 +1,38 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, nanoid, type PayloadAction } from "@reduxjs/toolkit";
 
 const initialData = {
   names: "jabo",
   email: "arnoldjabo@gmail.com",
-  location: {
-    city: "",
-    district: "",
-  },
+  location: { city: "", district: "" },
 };
+type newMe = Omit<typeof initialData, "id">;
+type Location = { city: string; district: string };
+
 const userSlice = createSlice({
   name: "userdata",
   initialState: initialData,
   reducers: {
-    addLocation(state, action: PayloadAction<Record<string, string>>) {
+    addLocation(state, action: PayloadAction<Location>) {
       state.location = {
         city: action.payload.city,
         district: action.payload.district,
       };
     },
+
+    addNewUser: {
+      reducer: (state, action: PayloadAction<newMe>) => {
+        return action.payload;
+      },
+
+      prepare: (payload: newMe) => ({
+        payload: {
+          id: nanoid(),
+          ...payload,
+        },
+      }),
+    },
   },
 });
-export const { addLocation } = userSlice.actions;
+export const { addLocation, addNewUser } = userSlice.actions;
 
 export default userSlice.reducer;
