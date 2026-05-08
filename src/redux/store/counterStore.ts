0f1a1d74e4a29736191dc46e74a9079d4bce6f21
jabo <arnoldjabo@gmail.com>
2026-05-08 23:18:27 +0200

    Added a new API call query for fetching a paginated URL from the server

--- src/redux/store/counterStore.ts
+++ src/redux/store/counterStore.ts
@@ -2,15 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../slices/counterSlice";
 import userReducer from "../slices/userSlice";
 import { apiCreated } from "../slices/apiSlice";
+import { fetchedData } from "../slices/myOwnSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     user: userReducer,
     [apiCreated.reducerPath]: apiCreated.reducer,
+    [fetchedData.reducerPath]: fetchedData.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiCreated.middleware),
+    getDefaultMiddleware().concat(fetchedData.middleware),
 });
 
 export type DispatchType = typeof store.dispatch;
